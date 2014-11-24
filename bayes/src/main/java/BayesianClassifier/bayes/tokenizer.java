74fec7c82e8a2924544b7ac23a0ad06ac6f7d317
FrankBloo <f.w.c.bloo@student.utwente.nl>
2014-11-24 11:01:24 +0100

    Project inclusive tokenizer

---
+++ bayes/src/main/java/BayesianClassifier/bayes/tokenizer.java
@@ -0,0 +1,14 @@
+package BayesianClassifier.bayes;
+
+public class tokenizer {
+
+	public String[] tokens(String input) {
+		String[] tokens = input.split(" ");
+		int i;
+		for (i = 0; i <= tokens.length; i++) {
+			tokens[i] = tokens[i].toLowerCase();
+		}
+		return tokens;
+	}
+
+}
