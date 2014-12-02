c0582453b33a69b7891b8b94f771053a8e238eb7
Kevin Alberts <kevin@kevinalberts.nl>
2014-12-02 10:30:48 +0100

    Changed start method to be a constructor and added a initialization field.

--- bayes/src/main/java/BayesianClassifier/bayes/BayesClassifier.java
+++ bayes/src/main/java/BayesianClassifier/bayes/BayesClassifier.java
@@ -10,37 +10,41 @@ import java.util.Map;
 
 public class BayesClassifier {
 	public Map<Integer, String> dataSet;
-	
-	public void Start () {
-		try {
-			File file = new File("classifications.bys");
+	private String filename = null;
+	private boolean isInitialized = false;
+
+	public BayesClassifier(String filename){
+		this.filename = filename;
+
+		try{
+			File file = new File(filename);
 			if(file.exists()) {
 				FileInputStream fis = new FileInputStream(file);
 				ObjectInputStream ois = new ObjectInputStream(fis);
-				try {
-					dataSet = (Map<Integer, String>) ois.readObject();
-				} catch (ClassCastException ex) {
-					System.err.println("Can not read classifications.bys!");
-				}
-			}
-			else {
+				this.dataSet = (Map<Integer, String>) ois.readObject();
+				this.isInitialized = true;
+			}else{
 				//CREATE A NEW DATASET!!!
 			}
-		}
-		catch (ClassNotFoundException | IOException e) {
-			
+		}catch(ClassNotFoundException | IOException | ClassCastException e){
+			System.out.println("Crash. Error: "+e.getClass().getName()+": "+e.getLocalizedMessage());
+			System.exit(1);
 		}
 	}
+
+	public boolean getInitializedStatus(){
+		return this.isInitialized;
+	}
 	
-	public void Exit () {
+	public void Exit() {
 		try {
-			File file = new File("classifications.bys");
+			File file = new File(this.filename);
 			if(file.exists()) {
 				file.delete();
 			}
 			FileOutputStream fis = new FileOutputStream(file);
 			ObjectOutputStream oos = new ObjectOutputStream(fis);
-			oos.writeObject(dataSet);
+			oos.writeObject(this.dataSet);
 			oos.flush();
 		}
 		catch (IOException e) {
