91231cbdca9cbb53affc43786a9ee99d640f79f3
Wouter van Agen <w.vanagen@student.utwente.nl>
2014-12-19 23:25:02 +0100

    Commented version of the ARFF generator

--- bayes/src/main/java/BayesianClassifier/bayes/Arff/NewArffGenerator.java
+++ bayes/src/main/java/BayesianClassifier/bayes/Arff/NewArffGenerator.java
@@ -16,71 +16,73 @@ public class NewArffGenerator {
 
 	public Instances createDataset(String directoryPath) throws IOException {
 
-		boolean firstRun = true;
-
 		FastVector atributes = new FastVector();
 
 		Instances data = new Instances("text_files_in_" + directoryPath,
 				(FastVector) atributes, 0);
 
+		// Open the directory and store each filename in a String
 		File directory = new File(directoryPath);
 		String[] files = directory.list();
 
+		// Loop throught each file contained in the subfolders of the directory
+		// specified in
+		// args[0]
 		for (int i = 0; i < files.length; i++) {
 			if (files[i].endsWith(".txt")) {
 				try {
 
+					// Open the file
 					File txt = new File(directoryPath + File.separator
 							+ files[i]);
 
 					InputStreamReader is;
 					is = new InputStreamReader(new FileInputStream(txt));
 					StringBuffer text = new StringBuffer();
+
 					int k;
 					while ((k = is.read()) != -1) {
-						text.append((char) k);
+						text.append((char) k); // add each letter in the text
+												// file to the StringBuffer
 
 					}
 
+					// Tokenize the text in the StringBuffer
 					String[] tokens = BayesianClassifier.bayes.Tokenizer
 							.tokens(text.toString());
 
+					// The array used to store the id of each word in the text
 					double[] newInst = new double[tokens.length];
 
 					// newInst[0] = (double)
 					// data.attribute(0).addStringValue(files[i]);
 
 					for (int j = 0; j < tokens.length; j++) {
-						// add the @attribute elements for each distinctive
+						// add the @attribute element for each distinctive
 						// word
 						if (!atributes.contains(new Attribute(tokens[j]))) {
 							atributes.addElement(new Attribute(tokens[j]));
 						}
+
+						// Get the id of the attribute associated with the word
+						// found
 						int temp = atributes.indexOf(new Attribute(tokens[j]));
 						String tempString = String.valueOf(temp);
 
+						// Add the id of the attribute associated with the word
+						// found to the @data section.
 						newInst[j] = (double) data.attribute(temp)
 								.addStringValue(tempString);
 
 						// System.out.println(tempString);
 						// System.out.println(newInst[j]);
 					}
-					/*
-					 * } else { String temp = ""; for (String t : tokens) { //
-					 * add the word variables to the @data segment
-					 * 
-					 * temp += atributes.indexOf(t);
-					 * 
-					 * } // data.add(new Instance(atributes.indexOf(new //
-					 * Attribute(t)), newInst));
-					 * 
-					 * 
-					 * }
-					 */
 
 					// newInst[1] = (double)
 					// data.attribute(1).addStringValue(text.toString());
 					data.add(new Instance(1.0, newInst));
+
+					is.close();
 				} catch (IOException e) {
 				}
 			}
